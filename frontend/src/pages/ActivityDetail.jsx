492bdac0f3d49af0b21d9d2ca13174632b09b8ff
Hritik <hritik.dev.04@gmail.com>
2026-04-07 00:06:37 +0530

    frontend files are updated

--- frontend/src/pages/ActivityDetail.jsx
+++ frontend/src/pages/ActivityDetail.jsx
@@ -85,7 +85,7 @@ export default function ActivityDetail() {
         return newSet;
       });
     });
-                  
+                           
     return () => {
       socket.disconnect();
     };
